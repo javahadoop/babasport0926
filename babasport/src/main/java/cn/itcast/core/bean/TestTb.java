a8e957b961a5a9a9b18c0ccf9982b600f43024b5
wqk <651518854@qq.com>
2015-09-19 01:58:13 +0800

    11

---
+++ babasport/src/main/java/cn/itcast/core/bean/TestTb.java
@@ -0,0 +1,35 @@
+package cn.itcast.core.bean;
+
+import java.util.Date;
+
+/**
+ * 测试类
+ * @author wqk
+ *
+ */
+public class TestTb {
+	private Integer id;
+	private String name;
+	private Date birthday;
+	public Integer getId() {
+		return id; 
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	
+	
+
+}
